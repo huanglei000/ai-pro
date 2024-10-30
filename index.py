3cca3bd5f946bdbe716b2485c497689bad5f6811
huanglei000 <2465858310@qq.com>
2024-10-30 22:01:20 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -5,21 +5,26 @@ import streamlit as st
 
 st.title("AI大模型应用产品网")
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("https://aigc-files.bigmodel.cn/api/cogview/20241029144859674acd6ce75a4ae2_0.png", use_column_width=True)
-    flag = st.button("黄磊绘言", use_container_width=True)
+    st.image("https://haowallpaper.com/link/common/file/getCroppingImg/8cab28d6df91aa2475acf2c7d4e25a0f", use_column_width=True)
+    flag = st.button("绘言", use_container_width=True)
     if flag:
         st.switch_page("pages/demo3.py")
 
 
 with col2:
-    st.image("https://aigc-files.bigmodel.cn/api/cogview/20241029144840b199d466b19e478a_0.png", use_column_width=True)
-    flag = st.button("黄磊绘图", use_container_width=True)
+    st.image("https://haowallpaper.com/link/common/file/getCroppingImg/83b9bb7e36e0640f150b5a1ad49d316683b9bb7e36e0640f150b5a1ad49d3166", use_column_width=True)
+    flag = st.button("绘图", use_container_width=True)
     if flag:
         st.switch_page("pages/textToimage.py")
 
+with col3:
+    st.image("https://haowallpaper.com/link/common/file/getCroppingImg/5b3b11f60ea2bbfa3063e6b4a3854e53", use_column_width=True)
+    flag = st.button("岗位问答", use_container_width=True)
+    if flag:
+        st.switch_page("pages/job-ai.py")
 
 
 
